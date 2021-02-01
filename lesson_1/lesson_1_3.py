ab2177764e65ef0cbefd6174afb2fa266d40f45f
yablockoo <yablockoo@gmail.com>
2021-02-01 15:36:26 +0300

    comment for #3

--- lesson_1/lesson_1_3.py
+++ lesson_1/lesson_1_3.py
@@ -1,3 +1,3 @@
 n_str = input("Введите число n: ")
-print(f"n + nn + nnn = {int(n_str) + int(2 * n_str) + int(3 * n_str)}")
+print(f"n + nn + nnn = {int(n_str) + int(2 * n_str) + int(3 * n_str)}")   # Можно через 3n + 2n * 10 + n * 100
 
