822a690c631ba4b541692c983e130aefdc9870ca
yablockoo <yablockoo@gmail.com>
2021-02-03 18:33:42 +0300

    2 №3 done

--- lesson_2/lesson_2_3.py
+++ lesson_2/lesson_2_3.py
@@ -1,1 +1,13 @@
-month = int(input("Введите номер месяца: "))
+month = int(input("Введите номер месяца (от 1 до 12): "))
+winter = [12, 1, 2]
+spring = [3, 4, 5]
+summer = [6, 7, 8]
+autumn = [9, 10, 11]
+seasons = {"Зима": winter, "Весна": spring, "Лето": summer, "Осень": autumn}
+
+for key, value in seasons.items():
+    if month in value:
+        print(f"Месяц №{month} это {key}")
+    else:
+        print("Неправильно введен месяц!")
+        break
