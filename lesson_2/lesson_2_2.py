9e93816f0c7e9416ccead68516edc5f6ce90f1de
yablockoo <yablockoo@gmail.com>
2021-02-03 17:15:02 +0300

    2 №2 done

--- lesson_2/lesson_2_2.py
+++ lesson_2/lesson_2_2.py
@@ -4,8 +4,6 @@ castling_list = input("Введите через пробел элементы �
 len_list = len(castling_list) if len(castling_list) % 2 == 0 else len(castling_list) - 1
 
 for i in range(0, len_list, 2):
-    var = castling_list[i]
-    castling_list[i] = castling_list[i + 1]
-    castling_list[i + 1] = var
+    castling_list[i], castling_list[i + 1] = castling_list[i + 1], castling_list[i]
 
 print(f"Ваш список перекручен вот так: {castling_list}")
