169a0c7d3213ad066c79aab416c37cf419b257c9
yablockoo <yablockoo@gmail.com>
2021-02-04 14:47:47 +0300

    2 №5 done

--- lesson_2/lesson_2_5.py
+++ lesson_2/lesson_2_5.py
@@ -1,13 +1,19 @@
-score_list = [1, 2, 5, 5, 1]
+score_list = []
 
 while True:
-    new_score = int(input("Введите новое чилсо: "))
+    new_score = int(input("Введите новое число: "))
+    # Вставка числа в случае пустотого списка
+    if len(score_list) == 0:
+        score_list.append(new_score)
+        print(score_list)
+        continue
 
-    if new_score not in score_list:
-        score_list.insert(0, new_score)
-    else:
-        new_pos = score_list.index(new_score)
-        new_pos += score_list.count(new_score)
-        score_list.insert(new_pos, new_score)
-
-    print(score_list.sort(key=))
+    for i in range(len(score_list)):
+        if new_score <= score_list[i]:   # Если введенное число меньше, то идем на след. шаг
+            if i == len(score_list) - 1:
+                score_list.append(new_score)   # Если число меньше всех чисел, вставка в конец
+            continue
+        else:
+            score_list.insert(i, new_score)   # Если число больше, вставка на тек. позицию
+            break
+    print(score_list)
