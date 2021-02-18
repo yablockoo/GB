fb4dd8c35cb0a204aa88f95c13e2ee8da9a91261
yablockoo <yablockoo@gmail.com>
2021-02-18 16:09:51 +0300

    5 №2 fixed.

--- lesson_5/lesson_5_2.py
+++ lesson_5/lesson_5_2.py
@@ -1,7 +1,7 @@
 lines = 0
 words = 0
 
-with open('text_2.txt', 'r') as andrew:
+with open('text_2.txt', 'r', encoding="utf-8") as andrew:
     for line in andrew:
         words += len(line.split())
         lines += 1
