667b006ca0e7920d5aacd1d64eaef02deadaee42
yablockoo <yablockoo@gmail.com>
2021-02-21 21:08:13 +0300

    6 №3 done.

--- Python_origins/lesson_6/lesson_6_3.py
+++ Python_origins/lesson_6/lesson_6_3.py
@@ -1,16 +1,3 @@
-"""
-3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
-name, surname, position (должность), income (доход).
-Последний атрибут должен быть защищенным и ссылаться на словарь,
-содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
-Создать класс Position (должность) на базе класса Worker.
-В классе Position реализовать методы получения полного имени сотрудника
-(get_full_name) и дохода с учетом премии (get_total_income).
-Проверить работу примера на реальных данных
-(создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
-"""
-
-
 class Worker:
     def __init__(self, var_name, var_surname, var_position, var_income: dict):
         self.name = var_name
@@ -24,4 +11,10 @@ class Position(Worker):
         return f"{self.name} {self.surname}"
 
     def get_total_income(self):
-        return f"{self.__income.get('wage') + self.__income.get('bonus')}"
+        return f"{self._Worker__income.get('wage') + self._Worker__income.get('bonus')}"
+
+
+p = Position("Иван", "Иванов", "старший помощник", {'wage': 10000, 'bonus': 5000})
+print(p.name, p.surname, p.position, p._Worker__income)   # на доступ жалуется, расскажите на уроке норма ли это
+print(p.get_full_name())
+print(f"{p.get_total_income()}р")
