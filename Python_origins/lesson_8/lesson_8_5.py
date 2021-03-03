2a6531d1f5ad403b4b95417691c99ae789a287a8
yablockoo <yablockoo@gmail.com>
2021-03-04 01:05:45 +0300

    8 №5 done.

--- Python_origins/lesson_8/lesson_8_5.py
+++ Python_origins/lesson_8/lesson_8_5.py
@@ -10,9 +10,8 @@
 class OfficeEquipment:
     store_list = {}
 
-    def __init__(self, model, quantity, weight, coast):
+    def __init__(self, model, weight, coast):
         self.model = model
-        self.quantity = quantity
         self.weight = weight
         self.coast = coast
 
@@ -21,29 +20,98 @@ class OfficeEquipment:
 
 
 class Printer(OfficeEquipment):
-    def __init__(self, model, quantity, weight, coast, cartridge_name, lists_capacity):
-        super().__init__(model, quantity, weight, coast)
+    equip_list = {}
+
+    def __init__(self, model, weight, coast, cartridge_name, lists_capacity):
+        super().__init__(model, weight, coast)
         self.cartridge_name = cartridge_name
         self.lists_capacity = lists_capacity
 
+    def to_store(self, quantity):
+        self.equip_list["модель"] = self.model
+        self.equip_list["количество"] = quantity
+        OfficeEquipment.store_list["Принтеры"] = self.equip_list
+
+    # Ищет модель на складе, вычитает из склада количество, считает сумму отправленного товара
+    def send_to_company(self, quantity_to_send):
+        print(f"Отправка {quantity_to_send} принтеров модели {self.model}.")
+        for keys in OfficeEquipment.store_list.keys():
+            if keys == "Принтеры":
+                for equip_keys in OfficeEquipment.store_list.get(keys):
+                    if OfficeEquipment.store_list.get(keys).get(equip_keys) == self.model:
+                        if self.equip_list.get("количество") < quantity_to_send:
+                            print("На складе не хватает принтеров, отправка отменена.")
+                        else:
+                            self.equip_list["количество"] = self.equip_list.get("количество") - quantity_to_send
+                            total_coast = quantity_to_send * self.coast
+                            print(f"Отправлено {quantity_to_send} принтеров {self.model} на сумму {total_coast}")
+
 
 class Scanner(OfficeEquipment):
-    def __init__(self, model, quantity, weight, coast, resolution):
-        super().__init__(model, quantity, weight, coast)
+    equip_list = {}
+
+    def __init__(self, model, weight, coast, resolution):
+        super().__init__(model, weight, coast)
         self.resolution = resolution
+
+    def to_store(self, quantity):
+        self.equip_list["модель"] = self.model
+        self.equip_list["количество"] = quantity
+        OfficeEquipment.store_list["Сканеры"] = self.equip_list
+
+    # Ищет модель на складе, вычитает из склада количество, считает сумму отправленного товара
+    def send_to_company(self, quantity_to_send):
+        print(f"Отправка {quantity_to_send} сканеров модели {self.model}.")
+        for keys in OfficeEquipment.store_list.keys():
+            if keys == "Сканеры":
+                for equip_keys in OfficeEquipment.store_list.get(keys):
+                    if OfficeEquipment.store_list.get(keys).get(equip_keys) == self.model:
+                        if self.equip_list.get("количество") < quantity_to_send:
+                            print("На складе не хватает сканеров, отправка отменена.")
+                        else:
+                            self.equip_list["количество"] = self.equip_list.get("количество") - quantity_to_send
+                            total_coast = quantity_to_send * self.coast
+                            print(f"Отправлено {quantity_to_send} сканеров {self.model} на сумму {total_coast}")
 
 
 class Projector(OfficeEquipment):
-    def __init__(self, model, quantity,  weight, coast, contrast, pic_resolution):
-        super().__init__(model, quantity, weight, coast)
+    equip_list = {}
+
+    def __init__(self, model,  weight, coast, contrast, pic_resolution):
+        super().__init__(model, weight, coast)
         self.contrast = contrast
         self.pic_resolution = pic_resolution
 
+    def to_store(self, quantity):
+        self.equip_list["модель"] = self.model
+        self.equip_list["количество"] = quantity
+        OfficeEquipment.store_list["Проекторы"] = self.equip_list
 
-printer = Printer("hp", 1, 25, 15000, "f100", 500)
-scanner = Scanner("xerox", 2, 5, 3500, "4800*4800")
-projector = Projector("fuji", 3,  3, 45000, "20000:1", "2048*1024")
+    # Ищет модель на складе, вычитает из склада количество, считает сумму отправленного товара
+    def send_to_company(self, quantity_to_send):
+        print(f"Отправка {quantity_to_send} проекторов модели {self.model}.")
+        for keys in OfficeEquipment.store_list.keys():
+            if keys == "Проекторы":
+                for equip_keys in OfficeEquipment.store_list.get(keys):
+                    if OfficeEquipment.store_list.get(keys).get(equip_keys) == self.model:
+                        if self.equip_list.get("количество") < quantity_to_send:
+                            print("На складе не хватает проекторов, отправка отменена.")
+                        else:
+                            self.equip_list["количество"] = self.equip_list.get("количество") - quantity_to_send
+                            total_coast = quantity_to_send * self.coast
+                            print(f"Отправлено {quantity_to_send} проекторов {self.model} на сумму {total_coast}")
 
-printer.show_info("принтер")
-scanner.show_info("сканер")
-projector.show_info("проектор")
+
+printer = Printer("hp", 25, 15000, "f100", 500)
+scanner = Scanner("xerox", 5, 3500, "4800*4800")
+projector = Projector("fuji",  3, 45000, "20000:1", "2048*1024")
+
+
+printer.to_store(5)
+scanner.to_store(3)
+projector.to_store(6)
+print(f"СКЛАД {OfficeEquipment.store_list}")
+printer.send_to_company(2)
+scanner.send_to_company(4)
+projector.send_to_company(4)
+print(f"СКЛАД {OfficeEquipment.store_list}")
